37bbae485b2d049027c52a2cb73e2a636a865743
Seth <itz.saga@gmail.com>
2018-04-05 11:49:58 -0500

    Small refactor

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -3,8 +3,9 @@ import Restaurant from './Restaurant'
 
 class Restaurants extends Component {
   render () {
-    const restaurants = this.props.store.getState().restaurants.map((r, index) => {
-      return <Restaurant restaurant={r} key={index} store={this.props} />
+    const { store } = this.props
+    const restaurants = store.getState().restaurants.map((r, index) => {
+      return <Restaurant restaurant={r} key={index} store={store} />
     })
     return (
       <ul>
--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -5,7 +5,6 @@ class Reviews extends Component {
   render () {
     const { store, restaurantId } = this.props
     const resReviews = store.getState().reviews.filter(r => r.restaurantId === restaurantId)
-    console.log(resReviews)
     const reviews = resReviews.map((review, index) => {
       return <Review store={store} key={index} review={review} />
     })
